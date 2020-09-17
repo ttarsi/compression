7a158aa9228d566328068d7bfd6f2fda3619dd42
tyler t <tytarsi@gmail.com>
2020-09-17 01:25:38 -0700

    added comments

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+'''
+Adaptive binary arithmetic coding
+
+'''
+
 from collections import defaultdict
 from math import log2
 import sys
@@ -11,22 +16,31 @@ def bitgen(x):
             yield int((c & (0x80>>i)) != 0)
 
 
-def simple_encode(byts, n_bits):
-
+def adaptive_encode(byts, n_bits):
+    '''
+    decoding would need to start from the same dict...
+    much more efficient than storing weights or prob dist
+    '''
+    # start with 50/50 for each context
     freq = defaultdict(lambda: [1,2])
-    prev = [0]*n_bits
+
+    # initialize context, entropy
+    prev = [-1]*n_bits
     H = 0.
     bg = bitgen(byts)
 
     for b in bg:
+        # given context, calculate probability of b
         prev_n = tuple(prev[-n_bits:])
     
         p_1 = freq[prev_n][0] / freq[prev_n][1]
-        p_x = p_1 if b == 1 else 1.0 - p_1
+        p_b = p_1 if b == 1 else 1.0 - p_1
     
-        h_i = -log2(p_x)
+        h_i = -log2(p_b)
         H += h_i
-    
+
+        # adaptively update probabilities
+        # because of this, don't need to save prob dist in order to decode 
         freq[prev_n][0] += b == 1
         freq[prev_n][1] += 1
     
@@ -40,4 +54,4 @@ if __name__=="__main__":
     print(f"initial size: {sys.getsizeof(enwik4)} bytes")
 
     for n in [4,8,16,32]:
-        print(f"simple encoding with {n} bit history: {simple_encode(enwik4, n):.5} bytes")
+        print(f"adaptive encoding with {n} bit history: {adaptive_encode(enwik4, n):.5} bytes")
